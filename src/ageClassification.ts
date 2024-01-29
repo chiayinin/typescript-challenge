f1dfa8c3f6db6245f678b8ac535ff6868f39876f
chiayinin <chiayinin@gmail.com>
2024-01-29 21:10:35 +0800

    feat: ageClassification done.

--- src/ageClassification.ts
+++ src/ageClassification.ts
@@ -10,6 +10,16 @@
  * @param age - 一個需要被分類的年齡
  * @returns - 回傳年齡的分類結果
  */
+enum ClassifyAgeData {
+    Child = 'Child',
+    Teenager = 'Teenager',
+    Adult = 'Adult',
+    Senior = 'Senior'
+  };
 export function classifyAge(age: number): string {
     // 請在此處寫下你的程式碼
+    if(age <= 12) return ClassifyAgeData.Child;
+    else if(age >= 13 && age <= 17) return ClassifyAgeData.Teenager;
+    else if(age >= 18 && age <= 64) return ClassifyAgeData.Adult;
+    else return ClassifyAgeData.Senior;
 }
